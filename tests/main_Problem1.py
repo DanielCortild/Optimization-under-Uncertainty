b9a1679435765d5f9568e20c7affbcc0c72e05b8
Daniel Cortild <daniel@cortild.com>
2024-10-22 19:52:00 +0200

    Cleaned up code

---
+++ tests/main_Problem1.py
@@ -0,0 +1,44 @@
+###############
+## Problem 1 ##
+###############
+
+import sys
+sys.path.append('../src')
+from ElectricityInvestmentPlanning import Problem1
+
+print("-------------")
+print("| Problem 1 |")
+print("-------------")
+print()
+
+# Get Expected Value Solution
+x_EVS, EV, spent_time = Problem1.getEVS()
+print("EVS Solution X: ", x_EVS)
+print("EVS Value: ", EV, f"({round(spent_time, 2)}s)")
+print()
+
+# Get Expected Result
+EEV, spent_time = Problem1.getEEV(x_EVS)
+print("EEV Value: ", EEV, f"({round(spent_time, 2)}s)")
+print()
+
+# Get Wait-And-See
+WS, spent_time = Problem1.getWS()
+print("WS Value: ", WS, f"({round(spent_time, 2)}s)")
+print()
+
+# Get the Two-Stage Solution
+x_TS, TS, spent_time = Problem1.getTS()
+print("TS Solution X: ", x_TS)
+print("TS Value: ", TS, f"({round(spent_time, 2)}s)")
+print()
+
+# Compute the Expected Value of Perfect Information
+EVPI = TS - WS
+print("EVPI Value: ", EVPI)
+print()
+
+# Compute the Value of Stochastic Solution
+VSS = EEV - TS
+print("VSS Value: ", VSS)
+print()
