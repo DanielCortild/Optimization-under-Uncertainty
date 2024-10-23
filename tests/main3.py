bcaba73d99789c85e5e41b76f660afbb1c05a768
Daniel Cortild <daniel@cortild.com>
2024-10-23 20:18:57 +0200

    Added WS to 3

--- tests/main3.py
+++ tests/main3.py
@@ -38,6 +38,13 @@ def main():
     print(f"Samples: {samples}")
     print()
 
+    # Get the Wait-And-See solution
+    samples = params["WS_Samples"] # Number of LHS samples. Programs are separate.
+    WS, spent_time = Problem3.getWS(samples)
+    print("WS Value: ", WS, f"({round(spent_time, 2)}s)")
+    print(f"Samples: {samples}")
+    print()
+
     # Compute the TS solution using an L-Shaped algorithm
     samples = samples = params["TS_LShaped_Samples"] # Number of LHS samples. Programs are not separate.
     iterations = params["TS_LShaped_Iterations"] # Number of iterations for L-Shaped algorithm
@@ -45,6 +52,16 @@ def main():
     print("TS L-Shaped Solution X: ", x_TS_LShaped)
     print("TS L-Shaped Value: ", TS_LShaped, f"({round(time_spent, 2)}s)")
     print(f"Samples: {samples}, Iterations: {iterations}")
+    print()
+
+    # Compute the Expected Value of Perfect Information
+    EVPI = TS_LShaped - WS
+    print("EVPI Value: ", EVPI)
+    print()
+
+    # Compute the Value of Stochastic Solution
+    VSS = EEV - TS_LShaped
+    print("VSS Value: ", VSS)
     print()
 
 if __name__ == "__main__":
